f5478510f7b431dc5dea1f4bca68f000a32b291f
WebModerna <edgardogalletto@gmail.com>
2020-05-05 20:32:15 -0300

    Creando spinners y escuchadores de eventos para enviar los emails y resetear los campos.

--- email/js/app.js
+++ email/js/app.js
@@ -4,7 +4,8 @@ const email = document.getElementById("email");
 const asunto = document.getElementById("asunto");
 const mensaje = document.getElementById("mensaje");
 const btnEnviar = document.getElementById("enviar");
-
+const formularioEnviar = document.getElementById("enviar-mail");
+const resetBtn = document.getElementById("resetBtn");
 
 // Escuchadores
 eventListeners();
@@ -18,6 +19,12 @@ function eventListeners()
     email.addEventListener("blur", validarCampo);
     asunto.addEventListener("blur", validarCampo);
     mensaje.addEventListener("blur", validarCampo);
+
+    // Botón enviar en el submit
+    btnEnviar.addEventListener("click", enviarEmail);
+
+    // Botón de reset
+    resetBtn.addEventListener("click", resetFormulario);
 }
 
 // Funciones
@@ -45,6 +52,41 @@ function validarCampo()
     }
 }
 
+// Resetear el formulario
+function resetFormulario(e)
+{
+    e.preventDefault();
+    formularioEnviar.reset();
+}
+
+// Cuando se envía el correo
+function enviarEmail(e)
+{
+    e.preventDefault();
+
+    // Spinner al presionar Enviar
+    const spinnerGif = document.querySelector('#spinner');
+    spinnerGif.style.display = "block";
+    console.log(spinnerGif);
+
+    // gif que envía el email
+    const enviado = document.createElement("img");
+    enviado.src = "img/mail.gif";
+    enviado.style.display = "block";
+    
+    // Ocultar el spinner y mostrar de enviado
+    setTimeout(function(){
+        spinnerGif.style.display = "none";
+        document.querySelector("#loaders").appendChild(enviado);
+
+        setTimeout(function(){
+            enviado.remove();
+            formularioEnviar.reset();
+        }, 5000);
+    }, 3000)
+}
+
+// Verifica la longitur del texto en los campos
 function validarLongitud(campo)
 {
     if(campo.value.length > 0)
